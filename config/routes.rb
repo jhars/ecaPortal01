6512b586f1f9e77cf27f4bedfb83e0b183c15ee6
Jonathan Harlan <jjharlan06@gmail.com>
2016-08-09 13:14:56 -0400

    Navbar + Customer Controller

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'customer/index'
+
+  get 'customer/show'
+
+  get 'customer/new'
+
+  get 'customer/edit'
+
+  get 'customer/delete'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
