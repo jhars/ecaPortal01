af7150bac32f600a49baa5bcc08f36c73c99d89f
Jonathan Harlan <jjharlan06@gmail.com>
2016-08-16 17:56:20 -0400

    temp change required user_params, LOGIN WORKING

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-  	params.require(:user).permit(:email, :phone, :school_district, :school, :password, :grade, :password_confirmation)
+  	# params.require(:user).permit(:email, :phone, :school_district, :school, :password, :grade, :password_confirmation)
+  	params.require(:user).permit(:email, :phone, :password, :password_confirmation)
   end
 end
